7da65fffbafc7c6933748cce015ec35488ea8d6f
tnguye85 <tnguye85@vols.utk.edu>
2022-04-18 13:48:46 -0500

    auto load

--- web/js/index.js
+++ web/js/index.js
@@ -1,56 +1,56 @@
+$(document).ready(function () {
 
-const timeoutId = setTimeout(function(){
-    window.location.reload(1);
-}, 2000);
+    var images = "";
 
-var isDrawing = false;
-var startX, startY;
+    setInterval(update, 5000);
 
-window.onload = function(){
-    const curDate = new Date();
-    const curTime = `${curDate.getHours()}:${curDate.getMinutes()}:${curDate.getSeconds()}`
-    document.getElementById('update-time').innerHTML = curTime;
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    setupAndRegister(canvas, ctx)
-    setupClickEvents(canvas, ctx);
-}
+    function update() {
 
-function setupAndRegister(canvas, ctx){
-    var img = new Image();
-    img.src = '/0.jpg';
-    img.onload = function(){
-        fill_canvas(canvas, ctx, img);
+        $.get("./recent")
+        .done(function(response) { 
+            if (response != "")
+            {
+                let json = JSON.parse(response);
+                if (json[0].file == images)
+                    return;
+
+                insertImages(json[0].file);
+
+                var currentdate = new Date(); 
+                var datetime = currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds() + "&nbsp;"
+                + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear();
+                $('#update-time').html(datetime);
+            }
+        }).fail(function() { 
+        });
+
+        $.get("./temperature")
+        .done(function(response) { 
+            if (response != "")
+            {
+                let json = JSON.parse(response);
+
+                setTemperature(json[0], json[1]);
+            }
+        }).fail(function() { 
+        });
     }
-}
 
-function fill_canvas(canvas, ctx, img){
-    // CREATE CANVAS CONTEXT.
-    let asr = img.width / img.height;
-
-    var width = window.innerWidth*0.4;
-    var height = width / asr;
-    canvas.width = width;
-    canvas.height = height;
-    ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, width, height);       // DRAW THE IMAGE TO THE CANVAS.
-}
-
-function setupClickEvents(canvas, ctx){
-    canvas.onmousedown = function(e){
-        let mouseX = e.offsetX;
-        let mouseY = e.offsetY;
-        if(isDrawing){
-            isDrawing=false;
-            ctx.beginPath();
-            ctx.rect(startX,startY,mouseX-startX,mouseY-startY);
-            ctx.fill();
-            canvas.style.cursor="default";
-        }else{
-            isDrawing=true;
-            startX=mouseX;
-            startY=mouseY;
-            canvas.style.cursor="crosshair";
-            clearTimeout(timeoutId);
+    function insertImages(url) {
+        for (let i = 4; i > 0; i--) {
+            $('#img'+i+">img").attr('src', $('#img'+(i-1)+">img").attr('src'));
         }
+        $('#img0>img').attr('src', $('#imgCurrent').attr('src'));
+        $('#imgCurrent').attr('src',url);
+        images = url;
     }
-}
+
+    function setTemperature(min, max) {
+        $('#inp_min').val(min);
+        $('#inp_max').val(max);
+    }
+});
